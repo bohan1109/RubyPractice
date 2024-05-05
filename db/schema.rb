54bd134134c2125420511c21110ac55774006964
Hank <4A790162@stust.edu.tw>
2024-05-06 03:14:05 +0800

    Run migration to create Scores table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_05_120019) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_05_150918) do
+  create_table "scores", force: :cascade do |t|
+    t.integer "math"
+    t.integer "english"
+    t.integer "chinese"
+    t.integer "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_scores_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "scores", "students"
 end
